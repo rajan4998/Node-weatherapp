df42a42b06b036c5b2a3e1a9c9cd1b3fcdb69f9b
rajan4998 <rajan4998@gmail.com>
2019-05-29 00:07:50 +0530

    darksky.net weather api added and tested

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 const request = require('request')
+
+const url = 'https://api.darksky.net/forecast/8ba53b38cc3194a7746c341bd7554374/37.8267,-122.4233'
+
+request({url : url} , (error,response) => {
+    console.log(response)
+})
