71faf2b52e88fba4705f50f4b6a7ec3c2bd4f23d
rajan4998 <rajan4998@gmail.com>
2019-05-29 06:58:09 +0530

    error handling added in API calls, missing updated config.js added

--- app.js
+++ app.js
@@ -5,16 +5,27 @@ const config = require('./config')
 const geocodeUrl = config.geocodeUrl
 
 request({ url : geocodeUrl,  json : true},(error,geoResponse) => {
-    const lat = geoResponse.body.features[0].center[1]
-    const lng = geoResponse.body.features[0].center[0]
-    console.log(lat+' '+lng)
-    const coordinates = lat + ',' + lng
 
-    const apiCoordinates = (coordinates != "") ? coordinates : config.coordinates
+    if(error){
+        console.log('Unable to connect to weather service')
+    }else{
 
-    const url = config.baseUrl+config.secretKey+'/'+apiCoordinates
+        const lat = geoResponse.body.features[0].center[1]
+        const lng = geoResponse.body.features[0].center[0]
+        const coordinates = lat + ',' + lng
+        const apiCoordinates = (coordinates != "") ? coordinates : config.coordinates
+        const url = config.baseUrl+config.secretKey+'/'+apiCoordinates
+    
+        request({url : url , json : true } , (weatherError,response) => {
+            if (weatherError){
+                console.log('Unable to connect to geocoding service')
+            }else{
+                // console.log(response.body.currently)
+                const forecast = response.body.currently
+                console.log(forecast.summary + '. It is currently ' + forecast.temperature + ' degree.There is ' + forecast.precipProbability+' chance of raining today.')
+            }
+        })
+    }
 
-    request({url : url , json : true } , (error,response) => {
-        console.log(response.body.currently)
-    })
+
 })
--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const config = {
     baseUrl: 'https://api.darksky.net/forecast/',
     secretKey: '8ba53b38cc3194a7746c341bd7554374',
-    coordinates: '28.702850,77.428749'
+    coordinates: '28.702850,77.428749',
+    geocodeUrl: 'https://api.mapbox.com/geocoding/v5/mapbox.places/Meerut.json?access_token=pk.eyJ1IjoicmFqYW40OTk4IiwiYSI6ImNpaGtha3RyYTBub3B2YmtsY3JieTljbXIifQ.AXJswCu_nMlcg4hSmBkABg&limit=1'
 }
 
 module.exports = config
