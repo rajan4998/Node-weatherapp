e712d9f5e24e85c6d221d9aede8ee885196ed821
rajan4998 <rajan4998@gmail.com>
2019-05-29 06:46:46 +0530

    geocode api url moved to config

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const request = require('request')
 
 const config = require('./config')
 
-
-
-const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Meerut.json?access_token=pk.eyJ1IjoicmFqYW40OTk4IiwiYSI6ImNpaGtha3RyYTBub3B2YmtsY3JieTljbXIifQ.AXJswCu_nMlcg4hSmBkABg&limit=1'
+const geocodeUrl = config.geocodeUrl
 
 request({ url : geocodeUrl,  json : true},(error,geoResponse) => {
     const lat = geoResponse.body.features[0].center[1]
@@ -17,11 +15,6 @@ request({ url : geocodeUrl,  json : true},(error,geoResponse) => {
     const url = config.baseUrl+config.secretKey+'/'+apiCoordinates
 
     request({url : url , json : true } , (error,response) => {
-        // w/o json : true in options array
-        // const formattedResponse = JSON.parse(response.body)
-        // console.log(formattedResponse.currently)
-    
-        // w/ json : true in options array
         console.log(response.body.currently)
     })
 })
