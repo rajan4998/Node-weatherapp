d2d1329647e4b32e6f3860a0178cd6ec00ab00ab
rajan4998 <rajan4998@gmail.com>
2019-05-29 15:02:50 +0530

    geocode file separated

--- app.js
+++ app.js
@@ -1,40 +1,68 @@
 const request = require('request')
 
 const config = require('./config')
+const geoCode = require('./utils/geocode')
 
 const geocodeUrl = config.geocodeUrl
 
-request({ url : geocodeUrl,  json : true},(error,geoResponse) => {
-
-    if(error){
-        console.log('Unable to connect to weather service')
-    }else{
-        if (geoResponse.body.features.length > 0){
-            const lat = geoResponse.body.features[0].center[1]
-            const lng = geoResponse.body.features[0].center[0]
-            const coordinates = lat + ',' + lng
-            let apiCoordinates = (coordinates != "") ? coordinates : config.coordinates
-            // apiCoordinates = "25.9999" //invalid coordinates
-            // apiCoordinates = "225.9999,225.9999"
-            const url = config.baseUrl+config.secretKey+'/'+apiCoordinates
+// request({ url : geocodeUrl,  json : true},(error,geoResponse) => {
+//     if(error){
+//         console.log('Unable to connect to weather service')
+//     }else{
+//         if (geoResponse.body.features.length > 0){
+//             const lat = geoResponse.body.features[0].center[1]
+//             const lng = geoResponse.body.features[0].center[0]
+//             const coordinates = lat + ',' + lng
+//             let apiCoordinates = (coordinates != "") ? coordinates : config.coordinates
+//             // apiCoordinates = "25.9999" //invalid coordinates
+//             // apiCoordinates = "225.9999,225.9999"
+//             const url = config.baseUrl+config.secretKey+'/'+apiCoordinates
         
-            request({url : url , json : true } , (weatherError,response) => {
-                if (weatherError){
-                    console.log('Unable to connect to geocoding service')
-                } else if (response.body.error) {
-                    console.log('Unable to find location')
-                } else if (response.body.currently!=null){
-                    const forecast = response.body.currently
-                    console.log(forecast.summary + '. It is currently ' + forecast.temperature + ' degree.There is ' + forecast.precipProbability+' chance of raining today.')
-                }
-                else{
-                    console.log("Config not set")
-                }
-            })
-        }else{
-            console.log("Coordinates not found for the location.")
-        }
-    }
+//             request({url : url , json : true } , (weatherError,response) => {
+//                 if (weatherError){
+//                     console.log('Unable to connect to geocoding service')
+//                 } else if (response.body.error) {
+//                     console.log('Unable to find location')
+//                 } else if (response.body.currently!=null){
+//                     const forecast = response.body.currently
+//                     console.log(forecast.summary + '. It is currently ' + forecast.temperature + ' degree.There is ' + forecast.precipProbability+' chance of raining today.')
+//                 }
+//                 else{
+//                     console.log("Config not set")
+//                 }
+//             })
+//         }else{
+//             console.log("Coordinates not found for the location.")
+//         }
+//     }
+// })
 
 
+//function geoCode calling
+geoCode('India', (error, response) => {
+    console.log('Error : '+error)
+    console.log(response)
+    // console.log('Response : ' + response.latitude)
 })
+
+// let apiCoordinates = config.coordinates
+
+// const url = config.baseUrl+config.secretKey+'/'+apiCoordinates
+
+// request({url : url , json : true } , (weatherError,response) => {
+//     if (weatherError){
+//         console.log('Unable to connect to geocoding service')
+//     } else if (response.body.error) {
+//         console.log('Unable to find location')
+//     } else if (response.body.currently!=null){
+//         const forecast = response.body.currently
+//         console.log(forecast.summary + '. It is currently ' + forecast.temperature + ' degree.There is ' + forecast.precipProbability+' chance of raining today.')
+//     }
+//     else{
+//         console.log("Config not set")
+//     }
+// })
+
+// weatherForecast('',()=>{
+
+// })
---
+++ utils/geocode.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+//function geoCode init
+const geoCode = (address, callback) => {
+    const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoicmFqYW40OTk4IiwiYSI6ImNpaGtha3RyYTBub3B2YmtsY3JieTljbXIifQ.AXJswCu_nMlcg4hSmBkABg&limit=1'
+
+    request({ url: geocodeUrl, json: true }, (error, geoResponse) => {
+        if (error) {
+            callback('Unable to connect to location services', undefined)
+        } else if (geoResponse.body.features.length == 0) {
+            callback('No coordinates found for the location', undefined)
+        } else if (geoResponse.body.features.length > 0) {
+            callback(undefined, {
+                latitude: geoResponse.body.features[0].center[0],
+                longitude: geoResponse.body.features[0].center[1],
+                location: geoResponse.body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geoCode
