63514afed8b86c72ae513fab8123c65fbde1dc56
rajan4998 <rajan4998@gmail.com>
2019-05-29 23:52:00 +0530

    yargs removed, taking input directly from cmd

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 //default packages
 const request = require('request')
-const yargs = require('yargs')
-
-yargs.version('1.1.1');
 
 //user defined packages
 const geoCode = require('./utils/geocode')
@@ -11,30 +8,14 @@ const weatherForecast = require('./utils/weatherforecast')
 //coordinates defined
 let apiCoordinates = '28.702850,77.428749'
 
-yargs.command({
-    command : 'forecast',
-    describe : 'Get weather forecast of a city/area',
-    builder : {
-        place : {
-            describe : 'This is required to get forecast against',
-            demandOption : true,
-            type : 'string'
-        }
-    },
-    handler : function(argv){
+//function geoCode calling
+geoCode(process.argv[2], (error, response) => {
+    console.log('Error : ' + error)
+    console.log(response)
 
-        //function geoCode calling
-        geoCode(argv.place, (error, response) => {
-            console.log('Error : ' + error)
-            console.log(response)
-
-            //function weatherforecast calling
-            weatherForecast(response.longitude, response.latitude, (error, response) => {
-                console.log('Error : ' + error)
-                console.log(response)
-            })
-        })
-    }
+    //function weatherforecast calling
+    weatherForecast(response.longitude, response.latitude, (error, response) => {
+        console.log('Error : ' + error)
+        console.log(response)
+    })
 })
-
-yargs.parse()
