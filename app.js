c026ff55c539dfb556050d90c41077b2e12a82c7
rajan4998 <rajan4998@gmail.com>
2019-05-29 07:18:24 +0530

    error handling done in location not found

--- app.js
+++ app.js
@@ -9,26 +9,28 @@ request({ url : geocodeUrl,  json : true},(error,geoResponse) => {
     if(error){
         console.log('Unable to connect to weather service')
     }else{
-
-        const lat = geoResponse.body.features[0].center[1]
-        const lng = geoResponse.body.features[0].center[0]
-        const coordinates = lat + ',' + lng
-        let apiCoordinates = (coordinates != "") ? coordinates : config.coordinates
-        apiCoordinates = "25.9999"
-        const url = config.baseUrl+config.secretKey+'/'+apiCoordinates
-    
-        request({url : url , json : true } , (weatherError,response) => {
-            if (weatherError){
-                console.log('Unable to connect to geocoding service')
-            }else{
-                if(response.body.error){
+        if (geoResponse.body.features.length > 0){
+            const lat = geoResponse.body.features[0].center[1]
+            const lng = geoResponse.body.features[0].center[0]
+            const coordinates = lat + ',' + lng
+            let apiCoordinates = (coordinates != "") ? coordinates : config.coordinates
+            // apiCoordinates = "25.9999" //invalid coordinates
+            // apiCoordinates = "225.9999,225.9999"
+            const url = config.baseUrl+config.secretKey+'/'+apiCoordinates
+        
+            request({url : url , json : true } , (weatherError,response) => {
+                if (weatherError){
+                    console.log('Unable to connect to geocoding service')
+                } else if (response.body.error) {
                     console.log('Unable to find location')
                 }else{
                     const forecast = response.body.currently
                     console.log(forecast.summary + '. It is currently ' + forecast.temperature + ' degree.There is ' + forecast.precipProbability+' chance of raining today.')
                 }
-            }
-        })
+            })
+        }else{
+            console.log("Coordinates not found for the location.")
+        }
     }
 
 
