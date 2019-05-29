e6349ae2784beccf7c121af9d19ea5a448c9f111
rajan4998 <rajan4998@gmail.com>
2019-05-29 23:22:05 +0530

    command line values accepted for place in weather forecast

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 //default packages
 const request = require('request')
+const yargs = require('yargs')
+
+yargs.version('1.1.1');
 
 //user defined packages
 const geoCode = require('./utils/geocode')
@@ -8,14 +11,30 @@ const weatherForecast = require('./utils/weatherforecast')
 //coordinates defined
 let apiCoordinates = '28.702850,77.428749'
 
-//function geoCode calling
-geoCode('New Delhi', (error, response) => {
-    console.log('Error : '+error)
-    console.log(response)
+yargs.command({
+    command : 'forecast',
+    describe : 'Get weather forecast of a city/area',
+    builder : {
+        place : {
+            describe : 'This is required to get forecast against',
+            demandOption : true,
+            type : 'string'
+        }
+    },
+    handler : function(argv){
 
-    //function weatherforecast calling
-    weatherForecast(response.longitude, response.latitude,(error,response)=>{
-        console.log('Error : '+error)
-        console.log(response)
-    })
+        //function geoCode calling
+        geoCode(argv.place, (error, response) => {
+            console.log('Error : ' + error)
+            console.log(response)
+
+            //function weatherforecast calling
+            weatherForecast(response.longitude, response.latitude, (error, response) => {
+                console.log('Error : ' + error)
+                console.log(response)
+            })
+        })
+    }
 })
+
+yargs.parse()
