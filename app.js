92b25d98a4c9c1056b4d220a13be5246a7c5357a
rajan4998 <rajan4998@gmail.com>
2019-05-29 07:08:58 +0530

    code refactoring and error condition handling

--- app.js
+++ app.js
@@ -13,16 +13,20 @@ request({ url : geocodeUrl,  json : true},(error,geoResponse) => {
         const lat = geoResponse.body.features[0].center[1]
         const lng = geoResponse.body.features[0].center[0]
         const coordinates = lat + ',' + lng
-        const apiCoordinates = (coordinates != "") ? coordinates : config.coordinates
+        let apiCoordinates = (coordinates != "") ? coordinates : config.coordinates
+        apiCoordinates = "25.9999"
         const url = config.baseUrl+config.secretKey+'/'+apiCoordinates
     
         request({url : url , json : true } , (weatherError,response) => {
             if (weatherError){
                 console.log('Unable to connect to geocoding service')
             }else{
-                // console.log(response.body.currently)
-                const forecast = response.body.currently
-                console.log(forecast.summary + '. It is currently ' + forecast.temperature + ' degree.There is ' + forecast.precipProbability+' chance of raining today.')
+                if(response.body.error){
+                    console.log('Unable to find location')
+                }else{
+                    const forecast = response.body.currently
+                    console.log(forecast.summary + '. It is currently ' + forecast.temperature + ' degree.There is ' + forecast.precipProbability+' chance of raining today.')
+                }
             }
         })
     }
